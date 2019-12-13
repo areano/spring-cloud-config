bd2f779001a9c340534b5862297daaf4e8d86299
Ariel Reano <ariel.reano@retailmerchantservices.co.uk>
2019-12-13 10:58:43 +0000

    Initial commit

---
+++ client/src/main/java/com/areano/spring/ClientApplication.java
@@ -0,0 +1,34 @@
+package com.areano.spring;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Log
+@EnableScheduling
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Component
+    class Scheduler {
+
+        @Value("${app.topic.rms-inbound}")
+        String name = "World";
+
+        @Scheduled(fixedDelay = 1000)
+        void debugProperty() {
+            log.info(name);
+        }
+
+    }
+
+
+}
