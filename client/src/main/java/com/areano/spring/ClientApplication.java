e4fdc0b60e5c6f65d1c0c7bd301b5a1c8e0d18a9
areano <areanho@gmail.com>
2019-12-13 13:25:04 +0000

    Enable config monitor

--- client/src/main/java/com/areano/spring/ClientApplication.java
+++ client/src/main/java/com/areano/spring/ClientApplication.java
@@ -1,15 +1,17 @@
 package com.areano.spring;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Log
-@EnableScheduling
 @SpringBootApplication
 public class ClientApplication {
 
@@ -18,16 +20,29 @@ public class ClientApplication {
     }
 
     @Component
+    @EnableScheduling
+    @RequiredArgsConstructor
     class Scheduler {
 
-        @Value("${info.app.environment}")
-        String name = "dev";
+        final DefaultService defaultService;
 
         @Scheduled(fixedDelay = 1000)
         void debugProperty() {
-            log.info(name);
+            defaultService.showEnvironment();
         }
 
     }
 
+    @Service
+    @RefreshScope
+    class DefaultService {
+
+        @Value("${info.app.environment}")
+        String environment = "dev";
+
+        void showEnvironment() {
+            log.info(environment);
+        }
+
+    }
 }
